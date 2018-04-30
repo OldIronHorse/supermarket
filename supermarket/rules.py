1e4d5b91b9d40f23235a81e1293c6e79eafb0fc3
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2018-04-30 08:20:17 +0100

    3 for 2 rule working

--- supermarket/rules.py
+++ supermarket/rules.py
@@ -1,12 +1,17 @@
 from functools import partial
+from .item import DiscountedItem
 
 def three_for_two(item):
   return partial(do_three_for_two, item)
     
 def do_three_for_two(item, basket):
-  #TODO switch to list comprehensions?
-  matching_items = filter(lambda i: i.name == item, basket)
-  non_matching_items = filter(lambda i: i.name != item, basket)
-  #TODO take_n instead of numeric approach?
+  print(basket)
+  full_price, discounted = basket
+  matching_items = [i for i in full_price if i.name == item]
+  non_matching_items = [i for i in full_price if i.name != item]
   (discount_count, spare) = divmod(len(matching_items), 3)
-  return (non_matching_items + matching_items[:spare], [])
+  new_discounts = [DiscountedItem(matching_items[0].name,
+                                  matching_items[0].price, 
+                                  0)] * discount_count
+  return (non_matching_items + matching_items[:spare], 
+          discounted + [matching_items[0]] * (discount_count * 2) + new_discounts)
--- supermarket/tests/test_supermarket.py
+++ supermarket/tests/test_supermarket.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, main
 from supermarket import make_receipt, PricedItem, Receipt, three_for_two, \
   DiscountedItem, price_items
 
@@ -21,7 +21,7 @@ class TestPriceItems(TestCase):
 
 class TestMakeReceipt(TestCase):
   def setUp(self):
-    self.maxDiff=None
+    self.maxDiff = None
     self.prices = {
      'soap': 1.5,
      'shampoo': 2.0,
@@ -36,29 +36,43 @@ class TestMakeReceipt(TestCase):
                              4.3),
                      make_receipt(basket, self.prices))
 
+class TestRule3For2(TestCase):
   def test_3_for_2_not_triggered_no_triplets(self):
-    basket = ['soap', 'shampoo', 'toothpaste']
-    self.assertEqual(Receipt([PricedItem('soap', 1.5), 
-                              PricedItem('shampoo', 2.0), 
-                              PricedItem('toothpaste', 0.8)],
-                             4.3),
-                     make_receipt(basket, self.prices, [three_for_two('soap')]))
+    basket = [PricedItem('soap', 1.5), 
+              PricedItem('shampoo', 2.0),
+              PricedItem('toothpaste', 0.8)]
+    full_price, discounted = three_for_two('soap')((basket, []))
+    self.assertFalse(discounted)
+    self.assertEqual(sorted([PricedItem('soap', 1.5),
+                             PricedItem('shampoo', 2.0),
+                             PricedItem('toothpaste', 0.8)]),
+                     sorted(full_price))
 
   def test_3_for_2_not_triggered_no_matching_triplets(self):
-    basket = ['soap', 'shampoo', 'shampoo', 'shampoo', 'toothpaste']
-    self.assertEqual(Receipt([PricedItem('soap', 1.5), 
-                              PricedItem('shampoo', 2.0), 
-                              PricedItem('shampoo', 2.0), 
-                              PricedItem('shampoo', 2.0), 
-                              PricedItem('toothpaste', 0.8)],
-                             8.3),
-                     make_receipt(basket, self.prices, [three_for_two('soap')]))
+    basket = [PricedItem('soap', 1.5), 
+              PricedItem('shampoo', 2.0),
+              PricedItem('shampoo', 2.0),
+              PricedItem('shampoo', 2.0),
+              PricedItem('toothpaste', 0.8)]
+    full_price, discounted = three_for_two('soap')((basket, []))
+    self.assertFalse(discounted)
+    self.assertEqual(sorted([PricedItem('soap', 1.5), 
+                      PricedItem('shampoo', 2.0), 
+                      PricedItem('shampoo', 2.0), 
+                      PricedItem('shampoo', 2.0), 
+                      PricedItem('toothpaste', 0.8)]),
+                     sorted(full_price))
 
   def test_3_for_2_simplest(self):
-    basket = ['soap', 'soap', 'soap']
-    self.assertEqual(Receipt([PricedItem('soap', 1.5), 
-                              PricedItem('soap', 1.5), 
-                              DiscountedItem('soap', 1.5, 0.0)],
-                             3.0),
-                     make_receipt(basket, self.prices, [three_for_two('soap')]))
+    basket = [PricedItem('soap', 1.5), 
+              PricedItem('soap', 1.5), 
+              PricedItem('soap', 1.5)]
+    full_price, discounted = three_for_two('soap')((basket, []))
+    self.assertFalse(full_price)
+    self.assertEqual(sorted([PricedItem('soap', 1.5), 
+                             PricedItem('soap', 1.5), 
+                             DiscountedItem('soap', 1.5, 0.0)]),
+                     sorted(discounted))
 
+if __name__ == '__main__':
+  main()
