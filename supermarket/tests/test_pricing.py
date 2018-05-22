861f5b75471f65597d5bfb5979baeb8e2bf982b5
Simon Redding <s1m0n.r3dd1ng@gmail.com>
2018-05-22 17:38:06 +0100

    pricing test class renamed

--- supermarket/tests/test_pricing.py
+++ supermarket/tests/test_pricing.py
@@ -1,7 +1,5 @@
-from unittest import TestCase, main
-from supermarket import make_receipt, PricedItem, Receipt, three_for_two, \
-  DiscountedItem, price_items, two_for, MultiBuy, cheapest_free, freebies, \
-  WeighedItem, PricedWeighedItem, spend_x_on_y_get_zpc_off
+from unittest import TestCase
+from supermarket import PricedItem, WeighedItem, PricedWeighedItem, price_items
 
 class TestPriceItems(TestCase):
   def setUp(self):
@@ -23,11 +21,3 @@ class TestPriceItems(TestCase):
     basket = [WeighedItem('banana', 0.454)]
     self.assertEqual([PricedWeighedItem('banana', 0.454, 80, 36)],
                      list(price_items(basket, self.prices)))
-  
-
-
-
-
-
-if __name__ == '__main__':
-  main()
